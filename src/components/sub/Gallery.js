1438b295c11c9dbf2885a9cad28433035911b12c
carrottechplus <blohzzang@gmail.com>
2023-07-09 19:23:42 +0900

    gallery 레이아웃 작업

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -20,7 +20,47 @@ function Gallery() {
 			setItem(json.data.photos.photo);
 		});
 	}, []);
-	return <Layout name={'Gallery'}></Layout>;
+
+	return (
+		<Layout name={'Gallery'}>
+			<div className='gallery_wrap'>
+				<div className='gallery_menu'>
+					<button type='button' className='btnInterest'>
+						Interest Gallery
+					</button>
+					<button type='button' className='btnMine on'>
+						My Gallery
+					</button>
+					<span className='gallery_menu__bg'></span>
+				</div>
+				<ul className='gallery_list'>
+					{Item.map((item, idx) => {
+						return (
+							<li className='item' key={idx}>
+								<div className='gallery_item'>
+									<img
+										src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_w.jpg`}
+										alt={item.title}
+										className='pic'
+									/>
+									<div className='gallery_item__profile gallery_profile'>
+										<p className='gallery_profile__img'>
+											<img
+												src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+												alt={item.title}
+											/>
+										</p>
+										<span className='gallery_profile__id'>{item.owner}</span>
+										<p className='gallery_profile__title'>{item.title === '' ? 'Have a good day !' : item.title}</p>
+									</div>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</Layout>
+	);
 }
 
 export default Gallery;
