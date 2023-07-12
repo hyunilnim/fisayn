d0f46ec82c798dd5271769175ed49bb50428ebec
carrottechplus <blohzzang@gmail.com>
2023-07-12 23:10:02 +0900

    community 페이지 폼내용 state 적용

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,7 +1,44 @@
+import { useEffect, useRef, useState } from 'react';
 import Layout from '../common/Layout';
 
 function Community() {
-	return <Layout name={'Community'}></Layout>;
+	const input = useRef(null);
+	const textarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+
+	const resetForm = () => {
+		input.current.value = '';
+		textarea.current.value = '';
+	};
+
+	const createPost = () => {
+		if (!input.current.value.trim() || !textarea.current.value.trim()) {
+			resetForm();
+			return alert('제목과 본문을 모두 입력해주세요.');
+		}
+		setPosts([...Posts, { title: input.current.value, content: textarea.current.value }]);
+		resetForm();
+	};
+
+	useEffect(() => {
+		console.log(Posts);
+	}, [Posts]);
+
+	return (
+		<Layout name={'Community'}>
+			<div className='inputBox'>
+				<input type='text' placeholder='제목을 입력하세요.' ref={input} />
+				<br />
+				<textarea placeholder='본문을 입력하세요.' ref={textarea}></textarea>
+				<br />
+
+				<button type='button'>cancel</button>
+				<button type='button' onClick={createPost}>
+					write
+				</button>
+			</div>
+		</Layout>
+	);
 }
 
 export default Community;
