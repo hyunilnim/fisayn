464d6558d6627ee1aa45ad22eee6f729174c4c34
carrottechplus <blohzzang@gmail.com>
2023-07-13 13:14:00 +0900

    menu 파일 생성 및 초기 작업

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,53 +1,66 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, NavLink } from 'react-router-dom';
+import Menu from './Menu';
+import { useRef } from 'react';
 
 function Header({ type }) {
+	const toggleMenu = useRef(null);
 	const active = 'on';
 	return (
-		// props로 전달되는 type값을 header의 class명으로 지정해서 스타일 분기처리
-		<header className={type}>
-			<div className='header_wrap'>
-				<h1 className='logo'>
-					<Link to='/'>LOGO</Link>
-				</h1>
-				<nav id='nav'>
-					<ul className='gnb'>
-						<li>
-							<NavLink to='/department' activeClassName={active}>
-								Department
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/community' activeClassName={active}>
-								Community
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/gallery' activeClassName={active}>
-								Gallery
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/youtube' activeClassName={active}>
-								Youtube
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/contact' activeClassName={active}>
-								Contact
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/member' activeClassName={active}>
-								Member
-							</NavLink>
-						</li>
-					</ul>
-				</nav>
-				<FontAwesomeIcon icon={faBars} />
-			</div>
-		</header>
+		<>
+			<header className={type}>
+				<div className='header_wrap'>
+					<h1 className='logo'>
+						<Link to='/'>LOGO</Link>
+					</h1>
+					<nav id='nav'>
+						<ul className='gnb'>
+							<li>
+								<NavLink to='/department' activeClassName={active}>
+									Department
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/community' activeClassName={active}>
+									Community
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/gallery' activeClassName={active}>
+									Gallery
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/youtube' activeClassName={active}>
+									Youtube
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/contact' activeClassName={active}>
+									Contact
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/member' activeClassName={active}>
+									Member
+								</NavLink>
+							</li>
+						</ul>
+					</nav>
+					<button
+						type='button'
+						className='menuOpen'
+						onClick={() => {
+							toggleMenu.current.toggle();
+						}}
+					>
+						<FontAwesomeIcon icon={faBars} />
+					</button>
+				</div>
+			</header>
+			<Menu ref={toggleMenu} />
+		</>
 	);
 }
 
---
+++ src/components/common/Menu.js
@@ -0,0 +1,20 @@
+import { forwardRef, useImperativeHandle, useState } from 'react';
+
+const Menu = forwardRef((props, ref) => {
+	const [Open, setOpen] = useState(false);
+
+	useImperativeHandle(ref, () => {
+		return { toggle: () => setOpen(!Open) };
+	});
+	return (
+		<>
+			{Open && (
+				<nav id='mobileMenu'>
+					<h1>LOGO</h1>
+				</nav>
+			)}
+		</>
+	);
+});
+
+export default Menu;
