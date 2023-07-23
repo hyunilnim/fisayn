dd52c5b69febacb06ee088e0b137a0f515e84a9c
carrottechplus <blohzzang@gmail.com>
2023-07-23 20:32:20 +0900

    modal에 framer motion 추가

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,4 +1,5 @@
 import { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Modal = forwardRef((props, ref) => {
 	const [Open, setOpen] = useState(false);
@@ -13,14 +14,32 @@ const Modal = forwardRef((props, ref) => {
 
 	return (
 		<>
-			{Open && (
-				<aside className='modal' ref={ref}>
-					<div className='con'>{props.children}</div>
-					<span className='close' onClick={() => setOpen(false)}>
-						close
-					</span>
-				</aside>
-			)}
+			<AnimatePresence>
+				{Open && (
+					<motion.aside
+						className='modal'
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1, transition: { duration: 0.5 } }}
+						exit={{ opacity: 0, scale: 0, transition: { duration: 0.3 } }}
+					>
+						<motion.div
+							initial={{ opacity: 0, scaleY: 0 }}
+							animate={{
+								opacity: 1,
+								scaleY: 1,
+								originY: 0,
+								transition: { ease: 'easeInOut', delay: 1, duration: 0.5 },
+							}}
+							className='con'
+						>
+							{props.children}
+						</motion.div>
+						<button type='button' className='close' onClick={() => setOpen(false)}>
+							close
+						</button>
+					</motion.aside>
+				)}
+			</AnimatePresence>
 		</>
 	);
 });
