85c337fea1ea3d3548400b3db62d87443a975e44
carrottechplus <blohzzang@gmail.com>
2023-07-24 12:38:14 +0900

    메인에 유튜브 미리보기 출력

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -2,7 +2,8 @@ import { memo } from 'react';
 import { useSelector } from 'react-redux';
 
 function Vids() {
-	useSelector((store) => console.log(store));
+	const Vids = useSelector((store) => store.youtube.data);
+	console.log(Vids);
 
 	return (
 		<section className='myScroll'>
@@ -18,7 +19,7 @@ function Vids() {
 					</div>
 					<div className='gallery_list_wrap'>
 						<ul className='gallery_list'>
-							<li>
+							{/* <li>
 								<a href='#' className='gallery_item'>
 									<p className='gallery_item__title'>
 										Considering Hiring Us? Our Policy on Website Proposals and PitchesConsidering Hiring Us? Our Policy
@@ -56,7 +57,33 @@ function Vids() {
 										<img src={`${process.env.PUBLIC_URL}/img/pic4.jpeg`} alt='' className='gallery_img' />
 									</div>
 								</a>
-							</li>
+							</li> */}
+
+							{Vids.map((vid, idx) => {
+								if (idx >= 3) return null;
+								return (
+									<li key={idx}>
+										<a href='#' className='gallery_item'>
+											<p className='gallery_item__title'>
+												{vid.snippet.title.length > 70 ? vid.snippet.title.substr(0, 90) + '...' : vid.snippet.title}
+											</p>
+											<div className='gallery_item__info gallery_info'>
+												<span className='gallery_info__name'>By {vid.snippet.videoOwnerChannelTitle}</span>
+												<span className='gallery_info__date'>
+													{vid.snippet.publishedAt.split('T')[0].split('-').join('.')}
+												</span>
+											</div>
+											<div className='gallery_item__img'>
+												<img
+													src={vid.snippet.thumbnails.standard.url}
+													alt={vid.snippet.title}
+													className='gallery_img'
+												/>
+											</div>
+										</a>
+									</li>
+								);
+							})}
 						</ul>
 					</div>
 				</div>
