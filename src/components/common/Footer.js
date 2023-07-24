287c22d225567af9f5269febaa26f1cecf16c7b6
carrottechplus <blohzzang@gmail.com>
2023-07-24 12:48:40 +0900

    footer컴포넌트에 department 데이터 연동

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,9 +1,14 @@
+import { useSelector } from 'react-redux';
+
 function Footer() {
+	const Department = useSelector((store) => store.department.data);
+
 	return (
 		<footer id='footer'>
 			<div className='inner footer_wrap'>
 				<h1>LOGO</h1>
 				<p>2023 DECODELAB &copy; ALL RIGHTS RESERVED.</p>
+				<p>{`This Company was founded by ${Department[0]?.name} in 1993`}</p>
 			</div>
 		</footer>
 	);
